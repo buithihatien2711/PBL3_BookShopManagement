8f7032308ef05a734d4935c723d691848c142d18
buithihatien2711 <buithihatien2711@gmail.com>
2021-05-31 10:32:12 +0700

    finish commit

--- PBL3_BookShopManagement/GUI/UserControls/UC_ManageKho.Designer.cs
+++ PBL3_BookShopManagement/GUI/UserControls/UC_ManageKho.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UC_ManageKho));
             this.panel5 = new System.Windows.Forms.Panel();
             this.rbtnTheoLinhVuc = new System.Windows.Forms.RadioButton();
             this.rbtnTheoLoaiSach = new System.Windows.Forms.RadioButton();
@@ -114,7 +113,6 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(21, 29);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(47, 48);
@@ -157,7 +155,6 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(21, 29);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(47, 48);
